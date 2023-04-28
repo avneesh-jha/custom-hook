1d909c8e27eaa83a1343a3e332c008df1d5cdf66
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-28 19:02:34 +0530

    completed

--- src/components/ImageListItems/ImageListItems.jsx
+++ src/components/ImageListItems/ImageListItems.jsx
@@ -1,4 +1,27 @@
 import "./ImageListItems.css";
+import { saveAs } from "file-saver";
 export function ImageListItem({ img }) {
-  return <img src={img.download_url} className="img" />;
+  async function downloadImage() {
+    const imageResponse = await fetch(img.download_url);
+    const imageBlob = await imageResponse.blob();
+    saveAs(imageBlob, img.author + "_" + img.id);
+  }
+
+  return (
+    <div className="card">
+      <a href={img.url}>
+        <img src={img.download_url} className="img" />;
+      </a>
+      <div className="card_banner">
+        <img src={img.download_url} className="card_thumb" />
+        <div className="card_text">
+          <h3 className="card_title">{img.author}</h3>
+          <div className="card_subtitle">
+            Original size : {img.height}x{img.width}
+          </div>
+          <button onClick={downloadImage}>Download</button>
+        </div>
+      </div>
+    </div>
+  );
 }
